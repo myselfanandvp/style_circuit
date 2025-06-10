89c63f3a7b9fdcca725b86c4567e999ee2a1173a
myselfanandvp <mailanandvp@gmail.com>
2025-06-11 03:05:33 +0530

    added the  admin list table

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import AdminHomeView
+from .views import AdminHomeView,AdminListView
 urlpatterns=[
     path('home/',AdminHomeView.as_view(),name='admin_home_url'),
+    path("list/",AdminListView.as_view(),name="admin_list_url")
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from usermanagement.models import CustomUser
+
 # Create your views here.
 
 @method_decorator(never_cache,name='dispatch')
@@ -10,3 +12,13 @@ class AdminHomeView(LoginRequiredMixin,View):
     login_url='login_url'
     def get(self,request):
         return render(request,'admin_views/home.html',{})
+    
+@method_decorator(never_cache,name='dispatch')
+class AdminListView(LoginRequiredMixin,View):
+    login_url='login_url'
+    def get(self,request):
+        users = CustomUser.objects.all()
+        if users is not None:            
+            return render(request,'admin_views/listuser.html',{'users':users})
+        return render(request,'admin_views/listuser.html',{'users':[]})
+        
