abea60303b84dda275b5354b3a6795e767e6f511
myselfanandvp <mailanandvp@gmail.com>
2025-06-16 22:01:11 +0530

    final commit

--- stylecircuit/middleware.py
+++ stylecircuit/middleware.py
@@ -1,6 +1,4 @@
 # middleware.py
-from django.http import HttpResponse
-from django.template.loader import render_to_string
 from django.shortcuts import render,redirect
 
 class Custom404Middleware:
